f8f25be772c0d499ff4708478a3108fa4706e620
Neru-neru <yumadachi0113soccer@gmail.com>
2022-02-04 13:39:01 +0900

    made gorm example, gitignore, env

--- main.go
+++ main.go
@@ -1,17 +1,50 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"os"
+	"log"
 
-	"github.com/gin-gonic/gin"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
+type Work struct {
+	gorm.Model
+	Title   string    `json:"title"`
+	Image      int    `json:"image"`
+	Url     string    `json:"url"`
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func gormConnect() *gorm.DB {
+	DBMS := "mysql"
+	USER := os.Getenv("DBUSER")
+	PASS := os.Getenv("PASSWORD")
+	PROTOCOL := "tcp(" + os.Getenv("DOMAIN") + ":" + os.Getenv("PORT") + ")"
+	DBNAME := os.Getenv("DBNAME") + "?parseTime=true&loc=Asia%2FTokyo"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+
+	db, err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("db connected: ", &db)
+	return db
+}
+
 func main() {
- r := gin.Default()
+	loadEnv()
+	db := gormConnect()
 
- r.GET("/hello", func(c *gin.Context) {
-  c.String(http.StatusOK, "Hello world")
-})
-
- r.Run(":8080")
+	defer db.Close()
+	db.LogMode(true)
 }
